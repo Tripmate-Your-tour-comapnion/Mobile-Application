fa31a19b885097a27b27c84cbd4bcf44bf6354a3
leoseven59 <leoseven59@gmail.com>
2024-05-15 11:01:19 +0000

    Changing the folder structure for the application

--- tripmate/lib/main.dart
+++ tripmate/lib/main.dart
@@ -14,6 +14,7 @@ class TripMateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TripMate',
       theme: ThemeData(
         useMaterial3: true,
