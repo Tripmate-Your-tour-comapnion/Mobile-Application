6ad47a14b19a2915809248d5f0b3d2de59b73766
yaikobzeray <yaikobzeray@gmail.com>
2024-05-18 08:27:50 +0300

    ADDING SETTING UI IS DONE

--- tripmate/lib/routes/routers.dart
+++ tripmate/lib/routes/routers.dart
@@ -1,4 +1,7 @@
 import 'package:tripmate/screens/regitration/Binding/registration_bindings.dart';
+import 'package:tripmate/screens/setting/Binding/edit_profile_Binding.dart';
+import 'package:tripmate/screens/setting/Binding/setting_binding.dart';
+import 'package:tripmate/screens/setting/edit_profile_screen.dart';
 import 'package:tripmate/screens/splash/Binding/splashScreenBinding.dart';
 
 import '../core/app_exports.dart';
@@ -9,22 +12,28 @@ import '../screens/onboarding/Binding/onboarding_two_binding.dart';
 import '../screens/onboarding/onboarding_one_screen.dart';
 import '../screens/onboarding/onboarding_two_screen.dart';
 import '../screens/regitration/registeration_screen.dart';
+import '../screens/setting/setting_screen.dart';
 
-class AppRoutes{
-
-
-static const String onboardingoneScreen = '/onboardingScreen_one';
-static const String onboardingtwoScreen = '/onboardingScreen_two';
-static const String registerationScreen = '/registrationScreen';
+class AppRoutes {
+  static const String editProfile = '/edit_profile_screen';
+  static const String settingScreen = '/setting_screen';
+  static const String onboardingoneScreen = '/onboardingScreen_one';
+  static const String onboardingtwoScreen = '/onboardingScreen_two';
+  static const String registerationScreen = '/registrationScreen';
 // static const String appNavigationScreen = '/app_navigation_screen';
-static const String initialRoute = '/initialRoutes';
-
-
-
-
-
+  static const String initialRoute = '/initialRoutes';
 
   static List<GetPage> pages = [
+    GetPage(
+      name: editProfile,
+      page: () => const EditProfileScreen(),
+      bindings: [EditProfileBinding()],
+    ),
+    GetPage(
+      name: settingScreen,
+      page: () => const SettingScreen(),
+      bindings: [SettingBinding()],
+    ),
     GetPage(
       name: initialRoute,
       page: () => const SplashScreeen(),
@@ -35,18 +44,15 @@ static const String initialRoute = '/initialRoutes';
       page: () => const OnboardingOneScreen(),
       bindings: [OnboardingOneBinding()],
     ),
-     GetPage(
+    GetPage(
       name: onboardingtwoScreen,
       page: () => const OnboardingTwoScreen(),
       bindings: [OnboardingTwoBinding()],
     ),
-       GetPage(
+    GetPage(
       name: registerationScreen,
       page: () => const RegistrationScreen(),
       bindings: [RegistrationBinding()],
     ),
- 
   ];
-
-
 }
